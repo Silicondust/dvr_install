cbeb46405c8d44d984d87cf882458a3ca2052036
Rik Dunphy <rik.dunphy@gmail.com>
2016-02-09 21:46:29 -0700

    fixed type on DVR_version

--- qnap/shared/ui/vars.php
+++ qnap/shared/ui/vars.php
@@ -1,12 +1,12 @@
 <?php
 final class DVRUI_Vars 
 {
-	const DVRUI_version    ="0.5.0";
+	const DVRUI_version    ="0.5.1";
 	const DVRUI_name       ="HDHomeRun DVR UI";
 	const DVRUI_author     ="Rik Dunphy";
 	const DVRUI_email      ="d3monrik@gmail.com";
 	const DVRUI_git        ="https://github.com/Silicondust/dvr_install";
-	const DVR_version      ="";
+	const DVR_version="20160208atest1";
 	const DVR_qpkgPath     ="/share/CACHEDEV1_DATA/.qpkg/HDHomeRunDVR";
 	const DVR_recPath      ="/share/Recordings/HDHomeRunDVR";
 	const DVR_config       ="HDHomeRunDVR.conf";
